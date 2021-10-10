78f769188063c72ee90e79015309c1bd063c6380
Ibrahim <ebrahiiim.kamal@gmail.com>
2021-10-10 19:53:53 +0200

    added

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,13 @@
-let cachData = 'appV1';
-
-this.addEventListener('install', event => {
-	event.waitUntil(
-		cache.addAll({
-			'/static/js/main.chunk.js',
-			'/static/js/O.chunk.js',
-			'/static/js/O.bundle.js',
-			'/index.html',
-			'/'
-		})
-		)
-})
+// let cachData = 'appV1';
+//
+// this.addEventListener('install', event => {
+// 	event.waitUntil(
+// 		cache.addAll({
+// 			'/static/js/main.chunk.js',
+// 			'/static/js/O.chunk.js',
+// 			'/static/js/O.bundle.js',
+// 			'/index.html',
+// 			'/'
+// 		})
+// 		)
+// })
